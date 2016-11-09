f45f5ad38a7d8812e578edc25eef4dfcc107a2b2
Ivan Ivankiv <ivan.ivankiv27@gmail.com>
2016-11-09 09:33:18 +0200

    Changed method to get date from the serve through state's resolve object

--- conFusion/Ionic/conFusion/www/js/controllers.js
+++ conFusion/Ionic/conFusion/www/js/controllers.js
@@ -152,23 +152,11 @@ angular.module('conFusion.controllers', [])
     };
   }])
 
-  .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 'baseURL', function($scope, $stateParams, menuFactory, baseURL) {
+  .controller('DishDetailController', ['$scope', '$stateParams', 'dish', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicPopover', '$ionicModal', function ($scope, $stateParams, dish, menuFactory, favoriteFactory, baseURL, $ionicPopover, $ionicModal) {
 
     $scope.baseURL = baseURL;
-    $scope.dish = {};
-    $scope.showDish = false;
-    $scope.message="Loading ...";
 
-    $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
-      .$promise.then(
-      function(response){
-        $scope.dish = response;
-        $scope.showDish = true;
-      },
-      function(response) {
-        $scope.message = "Error: "+response.status + " " + response.statusText;
-      }
-    );
+    $scope.dish = dish;
 
 
   }])
@@ -193,50 +181,43 @@ angular.module('conFusion.controllers', [])
 
   // implement the IndexController and About Controller here
 
-  .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', 'baseURL', function($scope, menuFactory, corporateFactory, baseURL) {
+  .controller('IndexController', ['$scope', 'menuFactory', 'promotionFactory', 'corporateFactory', 'baseURL', function ($scope, menuFactory, promotionFactory, corporateFactory, baseURL) {
 
     $scope.baseURL = baseURL;
-    $scope.leader = corporateFactory.get({id:3});
+    $scope.leader = corporateFactory.get({
+      id: 3
+    });
+
     $scope.showDish = false;
-    $scope.message="Loading ...";
-    $scope.dish = menuFactory.getDishes().get({id:0})
+    $scope.message = "Loading ...";
+
+    $scope.dish = menuFactory.get({
+      id: 0
+    })
       .$promise.then(
-      function(response){
+      function (response) {
         $scope.dish = response;
         $scope.showDish = true;
       },
-      function(response) {
-        $scope.message = "Error: "+response.status + " " + response.statusText;
+      function (response) {
+        $scope.message = "Error: " + response.status + " " + response.statusText;
       }
     );
-    $scope.promotion = menuFactory.getPromotion().get({id:0});
+
+    $scope.promotion = promotionFactory.get({
+      id: 0
+    });
+
   }])
 
-  .controller('FavoritesController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', '$ionicPopup', '$ionicLoading', '$timeout',
-      function ($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate, $ionicPopup, $ionicLoading, $timeout) {
+  .controller('FavoritesController', ['$scope', 'dishes', 'favorites', 'favoriteFactory', 'baseURL', '$ionicListDelegate', '$ionicPopup', '$ionicLoading', '$timeout', function ($scope, dishes, favorites, favoriteFactory, baseURL, $ionicListDelegate, $ionicPopup, $ionicLoading, $timeout) {
 
-  $scope.baseURL = baseURL;
-  $scope.shouldShowDelete = false;
+    $scope.baseURL = baseURL;
+    $scope.shouldShowDelete = false;
 
-  $ionicLoading.show({
-    template: '<ion-spinner></ion-spinner> Loading...'
-  });
+    $scope.favorites = favorites;
 
-  $scope.favorites = favoriteFactory.getFavorites();
-
-  $scope.dishes = menuFactory.getDishes().query(
-    function (response) {
-      $scope.dishes = response;
-      $timeout(function () {
-        $ionicLoading.hide();
-      }, 1000);
-    },
-    function (response) {
-      $scope.message = "Error: " + response.status + " " + response.statusText;
-      $timeout(function () {
-        $ionicLoading.hide();
-      }, 1000);
-    });
+    $scope.dishes = dishes;
 
     $scope.toggleDelete = function () {
       $scope.shouldShowDelete = !$scope.shouldShowDelete;
